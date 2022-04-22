084179af81e3bcc85515ca9052dc86530379237f
Zübeyir Bodur <zubeyir99@hotmail.com>
2022-04-23 01:39:07 +0300

    The dataset, and the initial plot script for explorary_data_analysis.py.

---
+++ src/explorary_data_analysis.py
@@ -0,0 +1,19 @@
+"""
+CS 484 - Introduction to Computer Vision - Term Project
+Genre Prediction for TV Series
+
+This script generates statistics plots
+regarding our dataset. By examining these
+plots, we will decide
+    - which genres should be estimated
+    - should genres be merged
+    - ...
+"""
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
